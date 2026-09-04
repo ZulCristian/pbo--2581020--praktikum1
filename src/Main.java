22f8bb12361faa6314c5921d3dec99a2041d475c
Zul Cristian Purba <2581020@unai.edu>
2026-09-04 17:10:03 +0700

    Menambahkan cidingan

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Biodata {
     public static void main(String[] args) {
         String nama = "Zul Cristian Purba";
-        String nim = "2581020";
-        String prodi = "Teknik Informatika";
-        String asalDaerah = "Sumatera Utara";
-        String alasanMemilihProdi = "Karena saya tertarik dengan teknologi dan pemrograman";
+        String nim = "";
+        String prodi = "";
+        String asalDaerah = "";
+        String alasanMemilihProdi = "";
         String targetSemester = "Mendapat nilai yang baik dan memahami fundamental dalam membuat sebuah program/coding";
         String hobi = "Bermain gitar dan badminton";
 
-        System.out.println("===== BIODATA MAHASISWA =====");
         System.out.println("Nama                  : " + nama);
         System.out.println("NIM                   : " + nim);
         System.out.println("Program Studi         : " + prodi);
