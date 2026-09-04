469c743f263d53051e8d439f9c61e5dd70e17713
Zul Cristian Purba <2581020@unai.edu>
2026-09-04 16:51:41 +0700

    Menambahkan codingan

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Zul Cristian Purba";
+        String nim = "2581020";
+        String prodi = "Teknik Informatika";
+        String asalDaerah = "Sumatera Utara";
+        String alasanMemilihProdi = "Karena saya tertarik dengan teknologi dan pemrograman";
+        String targetSemester = "Mendapat nilai yang baik dan memahami fundamental dalam membuat sebuah program/coding";
+        String hobi = "Bermain gitar dan badminton";
+
+        System.out.println("===== BIODATA MAHASISWA =====");
+        System.out.println("Nama                  : " + nama);
+        System.out.println("NIM                   : " + nim);
+        System.out.println("Program Studi         : " + prodi);
+        System.out.println("Asal Daerah           : " + asalDaerah);
+        System.out.println("Alasan Memilih Prodi  : " + alasanMemilihProdi);
+        System.out.println("Target Semester Ini   : " + targetSemester);
+        System.out.println("Hobi Saya             : " + hobi);
+    }
+}
